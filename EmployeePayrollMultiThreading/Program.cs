faa9fa5f11eaa9686e47f1179c41251c939a1dc1
pbishal <pbishal60@gmail.com>
2021-06-03 13:54:10 +0530

    Welcome to employee payroll Multithreading

---
+++ EmployeePayrollMultiThreading/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollMultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Emp Multithreading");
+        }
+    }
+}
